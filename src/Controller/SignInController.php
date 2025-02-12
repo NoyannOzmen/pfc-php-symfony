96c3af741b1c9c7287bb129239fe222098558d12
NoyannOzmen <noyann.ozmen@oclock.school>
2025-02-12 20:20:59 +0100

    Prepared flash notification in templates / First pass on login form & logic / First pass on Access Control + conditional links in Header

--- src/Controller/SignInController.php
+++ src/Controller/SignInController.php
@@ -1,18 +1,55 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SignInController extends AbstractController
 {
+/*   public function index(UserPasswordHasherInterface $passwordHasher): Response
+  {
+      // ... e.g. get the user data from a registration form
+      $user = new Utilisateur();
+      $plaintextPassword = ...;
 
-  #[Route('/connexion', name: 'login', methods: ['GET'])]
-    public function displayLogin(): Response
+      // hash the password (based on the security.yaml config for the $user class)
+      $hashedPassword = $passwordHasher->hashPassword(
+          $user,
+          $plaintextPassword
+      );
+      $user->setPassword($hashedPassword);
+  } */
+
+  public function index(): Response
+  {
+      // usually you'll want to make sure the user is authenticated first,
+      // see "Authorization" below
+      $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+      /** @var \App\Entity\Utilisateur $user */
+      $user = $this->getUser();
+
+      return new Response('Well hi there !');
+  }
+
+  #[Route('/connexion', name: 'login')]
+    public function displayLogin(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('signIn/connexion.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('signIn/connexion.html.twig', ['last_username' => $lastUsername,'error' => $error,]);
     }
+  
+  #[Route('/deconnexion', name: 'logout')]
+  public function logout()
+  {
+      throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+  }
 
   #[Route('/famille/inscription', name: 'foster_signup', methods: ['GET'])]
   public function displayFosterSignUp(): Response
